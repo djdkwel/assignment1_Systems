7f2877c7a16483abc3d6b9dc7dea3ed5332ab599
--global <--global>
2020-02-17 20:28:24 -0500

    Identify spelling error

--- assignment.c
+++ assignment.c
@@ -6,11 +6,12 @@ int main(){
     FILE *fp2;
     FILE *fp;
     //FILE *fp2;
-    char word[50000][30];
-    int i,j,k,l =0;
+    char word[50000];
+    int i,j,k,l,m =0;
     char dict[50000][30];
     char user[50000][30];
     int found = 0;
+    int n=0;
 
     //filename 
 
@@ -31,24 +32,33 @@ int main(){
             //strcpy(word[i],dict);
             j++;
         }
-    
+    int size = sizeof(user)/sizeof(int);
     //checks to see if user input is in dictionary
-    while(k<j){
-        
+    while(k<j){ 
         while(l<45427)
         {
             if(strcmp(user[k],dict[l])==0){
                 found = 1;
-                printf("%s \n",dict[l]);
+                //printf("%s \n",dict[l]);
+                m=l;
                 l=0;
                 
                 break;
-            } 
+            }
+             else if(l==45426){
+                 strcpy(word,user[k]);
+                 //printf("%s\n",word);
+                 n++;
+                 l=0;
+                 break;
+
+             }
             l++;
         }
             k++;
         }
         k =0;
+        printf(" %s\n",word[0]);
        /* 
        int result = strcmp(user[0],dict[308]);
        printf("%d",result);
